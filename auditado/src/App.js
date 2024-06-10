4ee5b376c652064011901250048d492040953d20
Fredy Esparza <fredyesparza08@gmail.com>
2024-06-10 15:46:21 -0600

    login auditados

--- auditado/src/App.js
+++ auditado/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { createContext } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login/loginForm" 
+import Inicio from './Components/Home/inicio';
+import AuthProvider from './AuthProvider';
+
+
+
+export const UserContext = createContext(null);
 
 function App() {
   return (
+    <AuthProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} /> {}
+          <Route path="/home" element={<Inicio/>}/> {}
+        </Routes>
+      </Router>
     </div>
+  </AuthProvider>
   );
 }
 
